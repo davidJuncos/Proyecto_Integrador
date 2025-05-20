13dc29f0a50b094c6785cd52e9d93870eb8c6425
davidjuncos <davidjuncos@hotmail.com>
2025-05-20 01:26:46 -0300

    agregar mas detalle de depuracion en componente Lista de producto

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,12 @@
 // src/components/ProductList.jsx
-function ProductList({ props }) {
-  if (props.length === 0) {
+function ProductList({ products }) {
+  if (products.length === 0) {
     return <div>No se encontraron productos</div>;
   }
 
   return (
     <ul>
-      {props.map((p) => (
+      {products.map((p) => (
         <li key={p.id}>
           {p.title} - ${p.price}
         </li>
