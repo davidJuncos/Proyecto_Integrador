698c07c680baa2e2ba46b447b0aea292a05f3f24
davidjuncos <davidjuncos@hotmail.com>
2025-05-20 00:20:59 -0300

    correccion de ocultar estado Estadistica

--- src/components/StatsPanel.jsx
+++ src/components/StatsPanel.jsx
@@ -1,4 +1,15 @@
-function StatsPanel() {
-  return <p>Estadisticas</p>;
+function StatsPanel(props) {
+  return (
+    <div>
+      <h2>Estadisticas</h2>
+      <p>Productos totales: {props.Total} productos</p>
+      {/*<p>            Productos con título mayor a 20 caracteres: {props.CantProd20 }          </p>{" "}*/}
+      {/*<p>Promedio de descuento: {props.PromDisc.toFixed(2)}%</p>*/}
+      {/* NUEVO */}
+      <p>Precio máximo: {props.max}</p>
+      <p>Precio mínimo: {props.mint}</p>
+      {/*<p>Precio total de productos filtrados: ${props.totalPrice.toFixed(2)}</p>*/}
+    </div>
+  );
 }
 export default StatsPanel;
